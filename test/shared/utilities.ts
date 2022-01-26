e94bbc14ced2deab57554a1603e1d447b1faacce
pichayut <pichayut@mbixtech.com>
2022-01-26 12:19:55 +0700

    update pragma

--- test/shared/utilities.ts
+++ test/shared/utilities.ts
@@ -1,78 +1,73 @@
-import { Contract, providers, utils, BigNumber} from 'ethers';
-import { MockProvider } from 'ethereum-waffle';
+import { Contract, utils, BigNumber } from "ethers";
+import { MockProvider } from "ethereum-waffle";
 
 const { keccak256, defaultAbiCoder, toUtf8Bytes, solidityPack } = utils;
 
-export const MINIMUM_LIQUIDITY = BigNumber.from(10).pow(3)
+export const MINIMUM_LIQUIDITY = BigNumber.from(10).pow(3);
 
-const PERMIT_TYPEHASH = keccak256(
-  toUtf8Bytes('Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)')
-)
+const PERMIT_TYPEHASH = keccak256(toUtf8Bytes("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"));
 
 export function toWei(n: number): BigNumber {
-  return BigNumber.from(n).mul(BigNumber.from(10).pow(18))
+  return BigNumber.from(n).mul(BigNumber.from(10).pow(18));
 }
 
 function getDomainSeparator(name: string, tokenAddress: string) {
   return keccak256(
     defaultAbiCoder.encode(
-      ['bytes32', 'bytes32', 'bytes32', 'uint256', 'address'],
+      ["bytes32", "bytes32", "bytes32", "uint256", "address"],
       [
-        keccak256(toUtf8Bytes('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)')),
+        keccak256(toUtf8Bytes("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)")),
         keccak256(toUtf8Bytes(name)),
-        keccak256(toUtf8Bytes('1')),
+        keccak256(toUtf8Bytes("1")),
         1,
-        tokenAddress
+        tokenAddress,
       ]
     )
-  )
+  );
 }
 
 export async function getApprovalDigest(
   token: Contract,
   approve: {
-    owner: string
-    spender: string
-    value: BigNumber
+    owner: string;
+    spender: string;
+    value: BigNumber;
   },
   nonce: BigNumber,
   deadline: BigNumber
 ): Promise<string> {
-  const name = await token.name()
-  const DOMAIN_SEPARATOR = getDomainSeparator(name, token.address)
+  const name = await token.name();
+  const DOMAIN_SEPARATOR = getDomainSeparator(name, token.address);
   return keccak256(
     solidityPack(
-      ['bytes1', 'bytes1', 'bytes32', 'bytes32'],
+      ["bytes1", "bytes1", "bytes32", "bytes32"],
       [
-        '0x19',
-        '0x01',
+        "0x19",
+        "0x01",
         DOMAIN_SEPARATOR,
         keccak256(
           defaultAbiCoder.encode(
-            ['bytes32', 'address', 'address', 'uint256', 'uint256', 'uint256'],
+            ["bytes32", "address", "address", "uint256", "uint256", "uint256"],
             [PERMIT_TYPEHASH, approve.owner, approve.spender, approve.value, nonce, deadline]
           )
-        )
+        ),
       ]
     )
-  )
+  );
 }
 
 export async function mineBlock(provider: MockProvider, timestamp: number): Promise<void> {
-  await new Promise(async (resolve, reject) => {
-    ;(provider.send as any)(
-      { jsonrpc: '2.0', method: 'evm_mine', params: [timestamp] },
-      (error: any, result: any): void => {
-        if (error) {
-          reject(error)
-        } else {
-          resolve(result)
-        }
+  await new Promise((resolve, reject) => {
+    (provider.send as any)({ jsonrpc: "2.0", method: "evm_mine", params: [timestamp] }, (error: any, result: any): void => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
       }
-    )
-  })
+    });
+  });
 }
 
 export function encodePrice(reserve0: BigNumber, reserve1: BigNumber) {
-  return [reserve1.mul(BigNumber.from(2).pow(112)).div(reserve0), reserve0.mul(BigNumber.from(2).pow(112)).div(reserve1)]
+  return [reserve1.mul(BigNumber.from(2).pow(112)).div(reserve0), reserve0.mul(BigNumber.from(2).pow(112)).div(reserve1)];
 }
